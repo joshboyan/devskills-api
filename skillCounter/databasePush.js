00272761a205439396a47e3b7dcac9927865c1b0
joshboyan <joshboyan@yahoo.com>
2017-09-12 11:31:38 -0700

    Refactor databasePush  to use mongoose driver

--- skillCounter/databasePush.js
+++ skillCounter/databasePush.js
@@ -2,39 +2,60 @@
  * This file connects to mongoDB and pushes the completed devskills object
  */
 const config = require('../config');
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
+
 
 const databasePush = devSkills => {
     'use strict';
-    
-    console.log('This got passed to database.js: ', devSkills.length);
-    // Use connect method to connect to the server
-    MongoClient.connect(config.dbURI, (err, db) => {
-        assert.equal(null, err);
-        console.log('Connected successfully to server');
 
-        insertCounter(db, function() {
-
-            db.close();
-        });
-
+    mongoose.connnect(config.dbURI, {
+        useMongoClient: true
     });
-    const insertCounter = (db, callback) => {
 
-        const collection = db.collection('crawlerTest');
-        const formattedSkills = {
-            date: new Date(),
-            skills: devSkills
-        };
+    // CONNECTION EVENTS
+    // When successfully connected
+    mongoose.connection.on('connected', function () {  
+        console.log('Mongoose default connection open to ' + config.dbURI);
+    }); 
 
-        collection.insert(formattedSkills, (err, result) => {
-            assert.equal(err, null);
-            assert.equal(1, result.result.n);
-            assert(1, result.ops.length);
-            callback(result);
-        });
-    }
+    // If the connection throws an error
+    mongoose.connection.on('error',function (err) {  
+        console.log('Mongoose default connection error: ' + err);
+    }); 
+
+    // When the connection is disconnected
+    mongoose.connection.on('disconnected', function () {  
+        console.log('Mongoose default connection disconnected'); 
+    });
+
+    // If the Node process ends, close the Mongoose connection 
+    process.on('SIGINT', function() {  
+        mongoose.connection.close(function () { 
+            console.log('Mongoose default connection disconnected through app termination'); 
+            process.exit(0); 
+        }); 
+    }); 
+    
+    // Import model
+    const Skill = require('../server/models/skill');
+
+    // Create a skill entry
+    const skill = new Skill();
+    skill.date = new Date();
+    skill.skills = devSkills;
+
+    // Add to database
+    skill.save(function(err){
+      if(err){
+        res.send(err);
+      } else {
+        res.json({message:"Skill added!",
+                  skill: skill});
+      }
+    });
+
+    console.log('This got passed to database.js: ', devSkills.length);
+    
 }
 
 module.exports = databasePush;
