c4c5c191506c81bbbe7e1679fb05b422b27c1f4b
joshboyan <joshboyan@yahoo.com>
2017-09-16 14:12:36 -0700

    Link docs to API

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,68 +2,81 @@ import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import './App.css';
 import 'react-bootstrap';
-import CircleVisualization from './CircleVisualization';
-import Skill from './Skill';
+import routeData from './data.json';
+import axios from 'axios';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      counts: [
-        {          
-          "_id": {
-              "$oid": "59b75b0e276a891410ffb042"
-          },
-          "date": {
-              "$date": "2017-09-12T03:57:02.684Z"
-          },
-          "skills": [
-            {
-                "name": "javascript",
-                "stackOverflow": 1467952,
-                "indeed": 168,
-                "twitter": 0
-            },
-            {
-                "name": "java",
-                "stackOverflow": 1310160,
-                "indeed": 199,
-                "twitter": 0
-            },
-            {
-                "name": "c#",
-                "stackOverflow": 1134760,
-                "indeed": 38,
-                "twitter": 0
-            },
-            {
-                "name": "php",
-                "stackOverflow": 1119502,
-                "indeed": 63,
-                "twitter": 0
-            }        
-          ]
-        }
-      ]  
+      routes: routeData,
+      selected: 0,
+      skills: [], 
+      results: {}
     }
+    this.handleFetch = this.handleFetch.bind(this);
+  }
+  handleFetch() {
+    axios.get(this.state.routes[this.state.selected].route)
+      .then(results => {
+        console.log('We fetched the data!', results);
+        this.setState({
+          results: results.data
+        });
+      }).catch(err => {
+        console.error('There was a problem fetching the data', err);
+      });
   }
   render() {
     return (
       <div className="App">
         <header className="dark-primary-color text-primary-color">
-          <h1><a href="/">DevSkills</a></h1>
-          <h4>Date</h4>
-          <h3><a href="#" target="_blank">API</a></h3>
+          <h1><a href="/">DevSkills API</a></h1>
+          <h3><a href="#" target="_blank">App</a></h3>
         </header>
         <main>
         <Row>
-          <Col xs={12} sm={8}>
-            <CircleVisualization />
-          </Col> 
           <Col xs={12} sm={4}>
-            <Skill props={this.state.counts[0].skills[0]}/>
+            <div>
+              <p onClick={ ()=> this.setState({selected: 0})}>Docs</p>
+              <hr />
+              <p>Routes</p>
+            </div>
+            <ul>
+              {this.state.routes.map((route, i) => {
+                return(
+                route.route ?
+                  <li key={i}
+                    onClick={ () => this.setState({selected: i})}>
+                    {route.route}
+                  </li> :
+                  null
+                )
+              })}
+            </ul>
+          </Col> 
+          <Col xs={12} sm={8}>
+            <h2>{this.state.routes[this.state.selected].route}</h2>
+            <p>{this.state.routes[this.state.selected].description}</p>
           </Col> 
         </Row>
+        <Row>
+          <Col xs={12}>
+          <h3>Try it out</h3>
+          <hr />
+              <div>fetch(https://devskillsapi.herokuapp.com{this.state.routes[this.state.selected].route})</div>
+              {this.state.selected > 3 ?
+              <select>
+                <option value="java">java</option>
+                <option value="python">python</option>
+                <option value="php">php</option>
+                <option value="aws">aws</option>
+              </select> : null }
+              <button onClick={ this.handleFetch }>Fetch Results</button>
+            <pre></pre>
+          </Col>
+        </Row>
+          
         </main>
       </div>
     );
--- server/server.js
+++ server/server.js
@@ -5,15 +5,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
 const countsRouter = require('./api/counts');
 const resoucesRouter = require('./api/resources');
 const port = process.env.PORT || 3899;
 
 // App middleware
 app.use(morgan('dev'));
-app.use(express.static('react-ui/public'));
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.header("Content-Type",'application/json');
+  next();
+});
 
 const mongoose = require('mongoose');
 mongoose.connect(config.dbURI, {
@@ -21,32 +27,49 @@ mongoose.connect(config.dbURI, {
 });
 
 // CONNECTION EVENTS
-    // When successfully connected
-    mongoose.connection.on('connected',  () =>  {  
-        console.log('Mongoose default connection open to ' + config.dbURI);
-    }); 
+// When successfully connected
+mongoose.connection.on('connected',  () =>  {  
+    console.log('Mongoose default connection open to ' + config.dbURI);
+}); 
 
-    // If the connection throws an error
-    mongoose.connection.on('error', err => {  
-        console.log('Mongoose default connection error: ' + err);
-    }); 
+// If the connection throws an error
+mongoose.connection.on('error', err => {  
+    console.log('Mongoose default connection error: ' + err);
+}); 
 
-    // When the connection is disconnected
-    mongoose.connection.on('disconnected',  () =>  {  
-        console.log('Mongoose default connection disconnected'); 
-    });
+// When the connection is disconnected
+mongoose.connection.on('disconnected',  () =>  {  
+    console.log('Mongoose default connection disconnected'); 
+});
 
-    // If the Node process ends, close the Mongoose connection 
-    process.on('SIGINT', () => {  
-        mongoose.connection.close( () =>  { 
-            console.log('Mongoose default connection disconnected through app termination'); 
-            process.exit(0); 
-        }); 
+// If the Node process ends, close the Mongoose connection 
+process.on('SIGINT', () => {  
+    mongoose.connection.close( () =>  { 
+        console.log('Mongoose default connection disconnected through app termination'); 
+        process.exit(0); 
     }); 
+}); 
+
+// Answer API requests.
+app.get('/api', function (req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send('{"message": "This is the base route for the DevSkills API. Remove directories from the URL to get to the docs."}');
+});
 
 // API routes
-app.use('/skills', countsRouter);
-app.use('/resouces', resoucesRouter)
+app.use('/api/skills', countsRouter);
+app.use('/api/resouces', resoucesRouter)
+
+// Serve the docs at the root URL
+app.get('/', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
+
 
 // Log errors
 app.use((err, req, res, next) => {
