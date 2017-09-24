f7c1384c96d44b24e809881a8c2dc72c0c2b717f
joshboyan <joshboyan@yahoo.com>
2017-09-24 16:41:27 -0700

    Add fucitonality tests and spies for UI

--- react-ui/src/__tests__/Response.test.js
+++ react-ui/src/__tests__/Response.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Response } from '../Response';
 import routeData from '../data.json';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -13,4 +14,34 @@ it('renders without crashing', () => {
 							results={ [] }
 							handleSelectChange={ null }
 							handleFetch={ null } />, div);
+});
+
+it('Calls handleSelectChange on click', function() {
+	const testFunc = jest.fn();
+	const wrapper = mount(<Response
+							selected={ 4 }
+							routes={ routeData }
+							skills={ ['java', 'c++'] }
+							selectedValue={ 'java' }
+							results={ [] }
+							handleSelectChange={ testFunc }
+							handleFetch={ null } />);
+	const select = wrapper.find('select');
+	select.simulate('change');
+	expect(testFunc.mock.instances.length).toBe(1);
+});
+
+it('Calls handleFetch on button click', function() {
+	const testFunc = jest.fn();
+	const wrapper = mount(<Response
+							selected={ 4 }
+							routes={ routeData }
+							skills={ ['java', 'c++'] }
+							selectedValue={ 'java' }
+							results={ [] }
+							handleSelectChange={ null }
+							handleFetch={ testFunc } />);
+	const button = wrapper.find('button');
+	button.simulate('click');
+	expect(testFunc.mock.instances.length).toBe(1);
 });
--- react-ui/src/__tests__/SideNav.test.js
+++ react-ui/src/__tests__/SideNav.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { SideNav } from '../SideNav';
 import routeData from '../data.json';
+import { mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -9,4 +10,15 @@ it('renders without crashing', () => {
 		routes={ routeData }
 		selected={ 0 }
 		updateSideNavLinks={ null } />, div);
+});
+
+it('Links call updateSideNavLinks when clicked', function() {
+	const testFunc = jest.fn()
+	const wrapper = mount(<SideNav
+		routes={ routeData }
+		selected={ 0 }
+		updateSideNavLinks={ testFunc } />);
+	const li = wrapper.find('li').first();
+	li.simulate('click');
+	expect(testFunc).toBeCalledWith({"selected": 1});
 });
